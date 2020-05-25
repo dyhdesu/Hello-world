9ac0e5480f574798ab52075bbee76cc84b379244
dyhdesu <958553176@qq.com>
2020-05-25 10:24:34 +0800

    tuple

--- Hello.py
+++ Hello.py
@@ -1,9 +1,9 @@
-list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
-tinylist = [123, 'runoob']
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
 
-print (list)            # 输出完整列表
-print (list[0])         # 输出列表第一个元素
-print (list[1:3])       # 从第二个开始输出到第三个元素
-print (list[2:])        # 输出从第三个元素开始的所有元素
-print (tinylist * 2)    # 输出两次列表
-print (list + tinylist) # 连接列表
+# 以下修改元组元素操作是非法的。
+# tup1[0] = 100
+
+# 创建一个新的元组
+tup3 = tup1 + tup2
+print(tup3)
