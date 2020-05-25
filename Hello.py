a9ce93aa8bee6979d663df83263ff334397cfb70
dyhdesu <958553176@qq.com>
2020-05-25 12:57:03 +0800

    tuple

--- Hello.py
+++ Hello.py
@@ -1,9 +1,14 @@
-tup1 = (12, 34.56)
-tup2 = ('abc', 'xyz')
+age = int(input("请输入你家狗狗的年龄: "))
+print("")
+if age <= 0:
+    print("你是在逗我吧!")
+elif age == 1:
+    print("相当于 14 岁的人。")
+elif age == 2:
+    print("相当于 22 岁的人。")
+elif age > 2:
+    human = 22 + (age - 2) * 5
+    print("对应人类年龄: ", human)
 
-# 以下修改元组元素操作是非法的。
-# tup1[0] = 100
-
-# 创建一个新的元组
-tup3 = tup1 + tup2
-print(tup3)
+### 退出提示
+input("点击 enter 键退出")
