f41a6d8124d6e0c537225451b295c44f7373bca3
dyhdesu <958553176@qq.com>
2020-06-02 11:20:33 +0800

    判断质数更新，活用.format

--- Hello.py
+++ Hello.py
@@ -6,11 +6,11 @@ if num > 1:
 
     for i in range(2,num):
         if (num % i) == 0:
-            print("{0}".format(num),"不是质数")
+            print("{0} 不是质数".format(num))
             print("{0} 乘 {1} 等于 {2}".format(i,num//i,num))
             break
     else:
-        print("{0}".format(num),"是质数")
+        print("{0} 是质数".format(num))
 
 else:
-    print("{0}".format(num),"不是质数")
+    print("{0} 不是质数".format(num))
